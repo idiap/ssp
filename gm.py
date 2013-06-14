f33f1634f56a09b4f8561862603a3eeca4e97f07
Phil Garner <Phil.Garner@idiap.ch>
2013-06-14 14:35:07 +0200

    Isolate the glottal pulses into raw functions

--- gm.py
+++ gm.py
@@ -11,6 +11,91 @@ import numpy.linalg as linalg
 
 from ssp import *
 
+def pulse_poly(pulse, Tp, Tn):
+    for i in range(Tp):
+        t = float(i)
+        pulse[i] = 3.0*(t/Tp)**2 - 2.0*(t/Tp)**3
+    for i in range(Tn):
+        t = float(i)
+        pulse[i+Tp] = 1.0 - (t/Tn)**2
+    return pulse
+
+def pulse_trig(pulse, Tp, Tn):
+    for i in range(Tp):
+        t = float(i)
+        pulse[i] = 0.5*(1.0-np.cos(np.pi*t/Tp))
+    for i in range(Tn):
+        t = float(i)
+        pulse[i+Tp] = np.cos(t/Tn * np.pi/2)
+    return pulse
+
+def pulse_gamma(pulse, Te, alpha, beta):
+    for i in range(int(Te)):
+        t = 1.0-float(i)/Te
+        pulse[i] = np.exp((alpha-1.0)*np.log(t) - t/beta)
+    return pulse
+
+def pulse_igamma(pulse, T, alpha, beta):
+    for i in range(int(T)):
+        t = 1.0-float(i)/T
+        pulse[i] = np.exp(-(alpha+1.0)*np.log(t) - beta/t)
+    return pulse
+
+def pulse_lf(pulse, T0, Te, alpha, omega, eps):
+    """
+    Assumes E_e = -1.
+    """
+    n = len(pulse)
+    for i in range(int(Te/T0*n+0.5)):
+        t = float(i)/n*T0
+        pulse[i] = -(
+            np.exp(alpha*t)  * np.sin(omega*t) /
+            np.exp(alpha*Te) / np.sin(omega*Te)
+            )
+    for i in range(int(Te/T0*n+0.5), n):
+        t = float(i)/n*T0
+        pulse[i] = -(
+            (np.exp(-(t-Te)*eps) - np.exp(-(T0-Te)*eps)) /
+            (1.0 - np.exp(-(T0-Te)*eps))
+            )
+    return pulse
+
+def lf_alpha(tp, te, epsilon, T0, alpha=0.0):
+    """
+    Given the three timing parameters and a starting point, uses
+    Newton-Raphson to find a value of alpha for the Liljencrants-Fant
+    glottal pulse shape.
+    """
+    tc = T0
+    omega = np.pi / tp
+    for iter in range(5):
+        exp = np.exp(-epsilon*(tc-te))
+        esin = np.exp(alpha*te)*np.sin(omega*te)
+        f = (
+            alpha
+            - omega/np.tan(omega*te)
+            + omega/esin
+            - (alpha**2 + omega**2)*((tc-te)*exp/(1.0-exp) - 1.0/epsilon)
+            )
+        fd = (
+            1.0
+            - omega*te/esin
+            - 2.0*alpha*((tc-te)*exp/(1.0-exp) - 1.0/epsilon)
+            )
+        alpha -= f/fd
+    return alpha
+
+def lf_epsilon(te, ta, T0):
+    tce = T0 - te
+    epsilon = 1.0/ta
+    for iter in range(5):
+        f = 1.0 - np.exp(-epsilon * tce) - ta * epsilon
+        fd = tce * np.exp(-epsilon * tce) - ta
+        epsilon -= f/fd
+
+    return epsilon
+
+
 class GlottalModel:
     """
     Class implementing various glottal modelling strategies.
@@ -64,23 +149,13 @@ class GlottalModel:
         elif self.ptype == 'poly':
             Tp = int(T * 0.4)
             Tn = int(T * 0.16)
-            for i in range(Tp):
-                t = float(i)
-                pulse[i] = 3.0*(t/Tp)**2 - 2.0*(t/Tp)**3
-            for i in range(Tn):
-                t = float(i)
-                pulse[i+Tp] = 1.0 - (t/Tn)**2
+            pulse_poly(pulse, Tp, Tn)
             if derivative:
                 pulse = ZeroFilter(pulse)
         elif self.ptype == 'trig':
             Tp = int(T * 0.4)
             Tn = int(T * 0.16)
-            for i in range(Tp):
-                t = float(i)
-                pulse[i] = 0.5*(1.0-np.cos(np.pi*t/Tp))
-            for i in range(Tn):
-                t = float(i)
-                pulse[i+Tp] = np.cos(t/Tn * np.pi/2)
+            pulse_trig(pulse, Tp, Tn)
             if derivative:
                 pulse = ZeroFilter(pulse)
         elif self.ptype == 'gamma':
@@ -90,17 +165,13 @@ class GlottalModel:
             Te = Tp*(Rk+1.0)
             alpha = 3.0
             beta  = 0.16/(alpha-1)
-            for i in range(int(Te)):
-                t = 1.0-float(i)/Te
-                pulse[i] = np.exp((alpha-1.0)*np.log(t) - t/beta)
+            pulse_gamma(pulse, Te, alpha, beta)
             if derivative:
                 pulse = ZeroFilter(pulse)
         elif self.ptype == 'igamma':
             alpha = 10
             beta  = 0.16*(alpha+1)
-            for i in range(int(T)):
-                t = 1.0-float(i)/T
-                pulse[i] = np.exp(-(alpha+1.0)*np.log(t) - beta/t)
+            pulse_igamma(pulse, T, alpha, beta)
             if derivative:
                 pulse = ZeroFilter(pulse)
         elif self.ptype == 'lf':
@@ -108,21 +179,10 @@ class GlottalModel:
             Ta = 1.0/(2.0*np.pi*self.Fa)
             Tp = T0/(2.0*self.Rg)
             Te = Tp*(self.Rk+1.0)
-            eps = self.lf_epsilon(Te, Ta, T0)
-            alpha = self.lf_alpha(Tp, Te, eps, T0)
+            eps = lf_epsilon(Te, Ta, T0)
+            alpha = lf_alpha(Tp, Te, eps, T0)
             omega = np.pi / Tp
-            for i in range(int(Te/T0*n+0.5)):
-                t = float(i)/n*T0
-                pulse[i] = -(
-                    np.exp(alpha*t)  * np.sin(omega*t) /
-                    np.exp(alpha*Te) / np.sin(omega*Te)
-                    )
-            for i in range(int(Te/T0*n+0.5), n):
-                t = float(i)/n*T0
-                pulse[i] = -(
-                    (np.exp(-(t-Te)*eps) - np.exp(-(T0-Te)*eps)) /
-                    (1.0 - np.exp(-(T0-Te)*eps))
-                    )
+            pulse_lf(pulse, T0, Te, alpha, omega, eps)
         elif self.ptype == 'invexp':
             for i in range(n):
                 t = 1.0-float(i/T)
@@ -152,41 +212,6 @@ class GlottalModel:
         # sample, so the power is the length of the pulse
         pulse *= np.sqrt(T) / linalg.norm(pulse)
         return pulse
-
-    def lf_alpha(self, tp, te, epsilon, T0, alpha=0.0):
-        """
-        Given the three timing parameters and a starting point, uses
-        Newton-Raphson to find a value of alpha for the Liljencrants-Fant
-        glottal pulse shape.
-        """
-        tc = T0
-        omega = np.pi / tp
-        for iter in range(5):
-            exp = np.exp(-epsilon*(tc-te))
-            esin = np.exp(alpha*te)*np.sin(omega*te)
-            f = (
-                alpha
-                - omega/np.tan(omega*te)
-                + omega/esin
-                -(alpha**2 + omega**2)*((tc-te)*exp/(1.0-exp) - 1.0/epsilon)
-                )
-            fd = (
-                1.0
-                - omega*te/esin
-                -2.0*alpha*((tc-te)*exp/(1.0-exp) - 1.0/epsilon)
-                )
-            alpha -= f/fd
-        return alpha
-
-    def lf_epsilon(self, te, ta, T0):
-        tce = T0 - te
-        epsilon = 1.0/ta
-        for iter in range(5):
-            f = 1.0 - np.exp(-epsilon * tce) - ta * epsilon
-            fd = tce * np.exp(-epsilon * tce) - ta
-            epsilon -= f/fd
-
-        return epsilon
 
 def GFilter(a, mag, angle, pole):
     """
