f79d8108cb6e3c9798c639ca05b2148d59c5b5af
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-06-25 09:22:20 +0200

    Spectrogram script: use argparse instead of optparse

--- ssp/script/spectrogram.py
+++ ssp/script/spectrogram.py
@@ -11,36 +11,48 @@
 from .. import ar
 from .. import core
 
-from optparse import OptionParser
+#from optparse import OptionParser
+import argparse
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
-  op = OptionParser()
-  (option, arg) = op.parse_args()
-  if (len(arg) < 1):
-      print "Need one arg"
-      exit(1)
-  file = arg[0]
+
+def command_line_arguments(command_line_parameters):
+  """Defines the command line parameters that are accepted."""
+
+  # create parser
+  parser = argparse.ArgumentParser(description='Computes the spectrogram of an audio signal (wav file only)', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('filename', type=file, help='Path to the file to process.')
+  parser.add_argument('-t', '--type', type=str, choices=('ar', 'psd', 'snr'), default='ar', help='The type of spectrogram to generate')
+  return parser.parse_args(command_line_parameters)
+
+
+def main(command_line_parameters = sys.argv):
+
+  # Collect command line arguments
+  args = command_line_arguments(command_line_parameters[1:])
 
   # Load and process
   pcm = core.PulseCodeModulation()
-  a = pcm.WavSource(file)
+  a = pcm.WavSource(args.filename)
   a = core.ZeroFilter(a)
   f = core.Frame(a, size=256, period=80)
-  type = core.parameter('Type', 'ar')
-  if type == 'psd':
-      p = core.Periodogram(f)
-      p = p[:,:p.shape[1]/2+1]
-  elif type == 'ar':
-      a = core.Autocorrelation(f)
-      a, g = ar.ARLevinson(a, pcm.speech_ar_order())
-      p = ar.ARSpectrum(a, g, nSpec=64)
-  elif type == 'snr':
-      p = core.Periodogram(f)
-      n = core.Noise(p)
-      p = core.SNRSpectrum(p, n)
-      p = p[:,:p.shape[1]/2+1]
+  ptype = core.parameter('Type', args.type)
+  if ptype == 'psd':
+    p = core.Periodogram(f)
+    p = p[:,:p.shape[1]/2+1]
+  elif ptype == 'ar':
+    a = core.Autocorrelation(f)
+    a, g = ar.ARLevinson(a, pcm.speech_ar_order())
+    p = ar.ARSpectrum(a, g, nSpec=64)
+  elif ptype == 'snr':
+    p = core.Periodogram(f)
+    n = core.Noise(p)
+    p = core.SNRSpectrum(p, n)
+    p = p[:,:p.shape[1]/2+1]
+  else:
+    raise runtime_error("Unsupported type for the spectrogram")
 
   # Draw it
   plt.bone()
@@ -49,3 +61,7 @@ def main():
   plt.show()
 
   return 0
+
+
+if __name__ == "__main__":
+  main()
